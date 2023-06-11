2de2b2379f65560e1e10b2ceb3b988fe6bfed8e6
damahouo <1254073041@qq.com>
2023-06-11 11:08:37 +0800

    项目初始化-引入组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
     <!-- 路由出口 -->
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-wenda"></i>
+      <van-button plain type="primary">朴素按钮</van-button>
+      <van-button plain type="info">朴素按钮</van-button>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import store from './store'
 // 加载全局样式
 import '@/styles/index.less'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
